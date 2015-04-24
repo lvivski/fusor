416147fc65468eae6934fda2d2466779a9ac6f66
Yehor Lvivski <lvivski@gmail.com>
2015-04-24 13:11:24 +0300

    Flexibility

--- flex.js
+++ flex.js
@@ -66,7 +66,7 @@
     return actions;
   };
   Flux.saveState = function(store) {
-    return JSON.stringify(obj.state);
+    return JSON.stringify(store.state);
   };
   Flux.restoreState = function(store, state) {
     store.emit(isObject(state) ? state : JSON.parse(state));
@@ -92,11 +92,18 @@
     this.controller = Stream.create(this);
     this.state = {};
   }
-  Store.prototype.emit = function(state) {
+  Store.prototype.get = Store.prototype.getState = function() {
+    return this.state;
+  };
+  Store.prototype.emit = Store.prototype.set = Store.prototype.setState = function(state) {
     for (var key in state) if (state.hasOwnProperty(key)) {
       this.state[key] = state[key];
     }
     return this.controller.add(state);
+  };
+  Store.prototype.clear = Store.prototype.clearState = function() {
+    this.state = {};
+    return this.controller.add(this.state);
   };
   Store.prototype.listen = function(callback) {
     return this.controller.stream.listen(callback);
--- src/store.js
+++ src/store.js
@@ -3,11 +3,24 @@ function Store() {
 	this.state = {}
 }
 
-Store.prototype.emit = function (state) {
+Store.prototype.get =
+Store.prototype.getState = function () {
+	return this.state
+}
+
+Store.prototype.emit =
+Store.prototype.set =
+Store.prototype.setState = function (state) {
 	for (var key in state) if (state.hasOwnProperty(key)) {
 		this.state[key] = state[key]
 	}
 	return this.controller.add(state)
+}
+
+Store.prototype.clear =
+Store.prototype.clearState = function () {
+	this.state = {}
+	return this.controller.add(this.state)
 }
 
 Store.prototype.listen = function (callback) {
